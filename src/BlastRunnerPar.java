7a5d30e41e40f3abc9c9cbf57d44ee26ae0b2919
Alexander Dean <dstar@csh.rit.edu>
2012-02-12 21:34:05 -0500

    Fixed Runners and added some tests. Cleaned things up.

--- src/BlastRunnerPar.java
+++ src/BlastRunnerPar.java
@@ -6,7 +6,9 @@
  **/
 
 import edu.rit.compbio.seq.Alignment;
-import edu.rit.compbio.seq.ProteinDatabase; // Reads in FASTA style databases.
+import edu.rit.compbio.seq.AlignmentPrinter;
+import edu.rit.compbio.seq.DefaultAlignmentStats;
+import edu.rit.compbio.seq.ProteinDatabase;
 import edu.rit.compbio.seq.ProteinSequence;
 import edu.rit.compbio.seq.Sequence;
 
@@ -17,6 +19,7 @@ import edu.rit.pj.WorkerLongForLoop;
 import edu.rit.mp.buf.ObjectItemBuf;
 
 import java.io.File;
+import java.util.Hashtable;
 
 public class BlastRunnerPar {
 
@@ -24,49 +27,88 @@ public class BlastRunnerPar {
         Comm.init(args);
         final Comm world = Comm.world();
         final int rank = world.rank();
+        final int size = world.size();
          
-        if( args.length != 4 ){ usage(); }
-        
-        final ProteinDatabase pd = new ProteinDatabase( new File( args[1] ) , new File("tmpIndexFile.dat") );
-        final float percentage = (new Float(args[2])).floatValue();
+        if( args.length != 5 ){ usage(); }
+        // TODO: ProteinDatabase wont necessarily work if the user passes in a nucleotide file in.
+        final ProteinDatabase pd = new ProteinDatabase( new File( args[1] ) , new File(args[2]) );
+        final float percentage = (new Float(args[3])).floatValue();
         
         final Sequence query;
         final BLAST aligner;
         if(args[0].toLowerCase().startsWith("p")){
-            query = new ProteinSequence(">Search Query", args[3]);
+            query = new ProteinSequence(">Search Query", args[4]);
             aligner = new BLASTP();
         }else{
-            query = new NucleotideSequence(">Search Query", args[3]);
+            query = new NucleotideSequence(">Search Query", args[4]);
             aligner = new BLASTN();
         }
-         
+        
+        long t1 = System.currentTimeMillis();
+        
         // Loop over database and find alignments.
+        final Hashtable<Sequence, Alignment[]> processData = new Hashtable<Sequence, Alignment[]>();
         new WorkerTeam().execute( new WorkerRegion(){
             public void run() throws Exception {
-                execute( 0L,  (long) (pd.getDatabaseLength()*percentage), new WorkerLongForLoop(){
+                execute( 0L,  (long) (pd.getProteinCount()*percentage), new WorkerLongForLoop(){
                     public void run( long first, long last ){
+                    	AlignmentPrinter out;
                         for( long i=first; i<=last; ++i){
                         	try{
 	                            Alignment[] tmp = aligner.align( query, pd.getProteinSequence( i ) );
-	                            if(rank != 0){
-	                            	world.send(0, new ObjectItemBuf<Alignment[]>(tmp));
+	                            
+	                            // If we are process 0, print as we find alignments. If we are any other process,
+	                            // hold our values until we finish all of them and then send it as a lump sum to
+	                            // process 0. (This conserves our message calls and reduces latency).
+	                            if(rank == 0){
+	                            	out = new AlignmentPrinter(System.out,new DefaultAlignmentStats(pd.getProteinSequence(i).length()));
+	                    			for(Alignment align : tmp)
+	                       	 			out.printDetails(align, query, pd.getProteinSequence( i ));
 	                            }else{
-	                            	//TODO: print alignments
+	                            	processData.put(pd.getProteinSequence( i ), tmp);
 	                            }
-                        	}catch( Exception e ){}
+                        	}catch( Exception e ){
+                        		//LATER: Logging?
+                        	}
                         }
+                    }
+                    
+                    public void finish() throws Exception{
+                    	ObjectItemBuf<Hashtable<Sequence, Alignment[]>> buf = new ObjectItemBuf<Hashtable<Sequence, Alignment[]>>();
+                    	buf.item = processData;
+                    	world.send(0, buf);
                     }
                });
              }
         });
         
+        // Listen for alignments from other processes when they are done.
+        if( rank == 0 ){
+        	AlignmentPrinter out;
+        	for(int i=1; i<=size; i++){
+        		ObjectItemBuf<Hashtable<Sequence, Alignment[]>> buf = new ObjectItemBuf<Hashtable<Sequence, Alignment[]>>();
+        		world.receive(i, buf); //TODO: validate CommStatus return?
+        		
+        		//Print sequences that this thread went over.
+        		for(Sequence subject : buf.item.keySet()){
+        			out = new AlignmentPrinter(System.out,new DefaultAlignmentStats(subject.length()));
+        			for(Alignment align : buf.item.get(subject))
+           	 			out.printDetails(align, query, subject);
+        		}
+        	}
+        }
+        
+        long t2 = System.currentTimeMillis();
+        System.out.printf("Running Time: %d msec%n", t2-t1);
+        
     }
 
     private static void usage(){
-        System.out.println( "Usage: java BlastRunnerSeq [p/n] <input database> <search coverage percentage> <query seqence>\n"+
+        System.out.println( "Usage: java BlastRunnerSeq [p/n] <input database> <index file> <search coverage percentage> <query seqence>\n"+
                             "\t[p/n] - p if proteins, n if nucleotides.\n"+
                             "\t<input database> - Path to a file with FASTA syle sequences.\n" +
-                            "\t<search coverage percentage> - A number 1-100, represents how much of the database to search\n"+
+                            "\t<database indexs> - Path to index file for input database.\n"+
+                            "\t<search coverage percentage> - A number 0.0-1.0, represents how much of the database to search\n"+
                             "\t<query seqence> - The seqence to search for in the input db.");
         System.exit(1);
     }
--- src/BlastRunnerSeq.java
+++ src/BlastRunnerSeq.java
@@ -5,6 +5,8 @@
  **/
 
 import edu.rit.compbio.seq.Alignment;
+import edu.rit.compbio.seq.AlignmentPrinter;
+import edu.rit.compbio.seq.DefaultAlignmentStats;
 import edu.rit.compbio.seq.ProteinDatabase; // Reads in FASTA style databases.
 import edu.rit.compbio.seq.ProteinSequence;
 import edu.rit.compbio.seq.Sequence;
@@ -14,37 +16,45 @@ import java.io.File;
 
 public class BlastRunnerSeq {
 
-    public static void main( String[] args ) throws Exception {
+    public static void main( String[] args ) throws Exception{
         Comm.init(args);
          
-        if( args.length != 4 ){ usage(); }
-        
-        ProteinDatabase pd = new ProteinDatabase( new File( args[1] ) , new File("tmpIndexFile.dat") );
-        float percentage = (new Float(args[2])).floatValue();
+        if( args.length != 5 ){ usage(); }
+
+        AlignmentPrinter out;
+        ProteinDatabase pd = new ProteinDatabase( new File( args[1] ) , new File(args[2]) );
+        float percentage = (new Float(args[3])).floatValue();
         
         Sequence query;
         BLAST aligner;
         if(args[0].toLowerCase().startsWith("p")){
-            query = new ProteinSequence("Search Query", args[3]);
+            query = new ProteinSequence(">Search Query", args[4]);
             aligner = new BLASTP();
         }else{
-            query = new NucleotideSequence("Search Query", args[3]);
+            query = new NucleotideSequence(">Search Query", args[4]);
             aligner = new BLASTN();
         }
         
-        for(long i=0; i < pd.getDatabaseLength()*percentage; i++){
+        long t1 = System.currentTimeMillis();
+        for(long i=0; i < pd.getProteinCount()*percentage; i++){
         	Alignment[] tmp = aligner.align( query, pd.getProteinSequence( i ) );
-            if(tmp.length>0){
-                //TODO: print the alignment nicely, for this subject sequence vs the query.
+        	// Print the alignments
+            for(int j=0; j<tmp.length;j++){
+            	 out = new AlignmentPrinter(System.out,new DefaultAlignmentStats(tmp[j].getSubjectLength()));
+            	 out.printDetails(tmp[j], query, pd.getProteinSequence(i));
             }
         }
+        long t2 = System.currentTimeMillis();
+        
+        System.out.printf("Running Time: %d msec%n", t2-t1);
     }
 
     private static void usage(){
-        System.out.println( "Usage: java BlastRunnerSeq [p/n] <input database> <search coverage percentage> <query seqence>\n"+
+        System.out.println( "Usage: java BlastRunnerSeq [p/n] <input database> <index file> <search coverage percentage> <query seqence>\n"+
                             "\t[p/n] - p if proteins, n if nucleotides.\n"+
+                            "\t<database indexs> - Path to index file for input database.\n"+
                             "\t<input database> - Path to a file with FASTA syle sequences.\n" +
-                            "\t<search coverage percentage> - A number 1-100, represents how much of the database to search\n"+
+                            "\t<search coverage percentage> - A number 0.0-1.0, represents how much of the database to search\n"+
                             "\t<query seqence> - The seqence to search for in the input db.");
         System.exit(1);
     }
